192114e0d8e81f474a691eb86a2be72446a8c805
221B228 <221B228@juet.co.in>
2025-02-28 14:23:44 +0530

    4 Th lab

---
+++ LE4/Lazy Solution/Main (1).java
@@ -0,0 +1,13 @@
+
+
+public class Main
+{
+	public static void main(String[] args) {
+		Singleton s1 = Singleton.getInstance();
+		System.out.println(s1.hashCode());
+		
+		Singleton s2 = Singleton.getInstance();
+		System.out.println(s2.hashCode());
+		
+	}
+}
